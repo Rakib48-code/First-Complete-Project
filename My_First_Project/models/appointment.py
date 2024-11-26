c54af500c44505d857d66a246b5416f0f0302f74
Rakib48-code <shahidulhuda0@gmail.com>
2024-11-26 12:41:05 +0600

    create appointment form view

--- My_First_Project/models/appointment.py
+++ My_First_Project/models/appointment.py
@@ -6,3 +6,5 @@ class HospitalAppointment(models.Model):
     _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient Name', required=True)
+    appointment_time = fields.Datetime(string='Appointment Time')
+    booking_date = fields.Date(string='Booking Date')
